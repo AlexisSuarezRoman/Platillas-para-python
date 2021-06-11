cbba77946c05a593a1cb73490e94c36e74406760
alexis suarez roman <84736316+AlexisSuarezRoman@users.noreply.github.com>
2021-06-10 20:52:55 -0500

    Update 04-funcion_anidada_que_tiene_como_parametro_otra_funcion.py

--- 02-decoradores/04-funcion_anidada_que_tiene_como_parametro_otra_funcion.py
+++ 02-decoradores/04-funcion_anidada_que_tiene_como_parametro_otra_funcion.py
@@ -1,7 +1,7 @@
 # funcion anidada que tiene como parametro una funcion.
 def funcion_envoltoria(funcion_parametro):
     print('codigo extra de la funcion envoltoria o wrapper')
-    funcion_parametro() # ---> llamado de la funcion parametro desde la funcion wrapper
+    funcion_parametro() # ---> llamado de la funcion parametro desde la funcion wrapper, en este ejemplo es funcion_comun.
     def funcion_a_envolver(): # ---> funcion anidada 
         print('codigo funcion anidada')
         funcion_parametro() # ---> llamado de la funcion parametro desde la funcion anidada
